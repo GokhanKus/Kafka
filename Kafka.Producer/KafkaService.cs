48d2aa141d254d53db7b0c560e968b6dcf2c12fd
Gokhan Kus <gkus1998@hotmail.com>
2024-10-30 12:28:11 +0300

    producer, SendSimpleMessageWithNullKey (10 messages)

--- Kafka.Producer/KafkaService.cs
+++ Kafka.Producer/KafkaService.cs
@@ -5,8 +5,7 @@ namespace Kafka.Producer
 {
 	internal class KafkaService
 	{
-		private const string TopicName = "mytopic";
-		internal async Task CreateTopicAsync()
+		internal async Task CreateTopicAsync(string topicName)
 		{
 			using var adminClient = new AdminClientBuilder(new AdminClientConfig
 			{
@@ -15,14 +14,45 @@ namespace Kafka.Producer
 			try
 			{
 				await adminClient.CreateTopicsAsync(new[]{
-				new TopicSpecification{Name = TopicName,NumPartitions = 3,ReplicationFactor = 1}
+				new TopicSpecification{Name = topicName,NumPartitions = 3,ReplicationFactor = 1}
 				});
-				Console.WriteLine($"topic ({TopicName}) olustu");
+				Console.WriteLine($"topic ({topicName}) olustu");
 			}
 			catch (Exception ex)
 			{
 				Console.WriteLine(ex.Message);
 			}
 		}
+		internal async Task SendSimpleMessageWithNullKey(string topicName)
+		{
+			var config = new ProducerConfig { BootstrapServers = "localhost:9094" };
+
+			using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+			foreach (var item in Enumerable.Range(1, 10))
+			{
+				var message = new Message<Null, string>()
+				{
+					Value = $"Message(use-case-1) - {item}"
+				};
+				var result = await producer.ProduceAsync(topicName, message);
+
+				foreach (var propertyInfo in result.GetType().GetProperties())
+				{
+					Console.WriteLine($"{propertyInfo.Name}:{propertyInfo.GetValue(result)}");
+				}
+				Console.WriteLine(new string('-', 50));
+				await Task.Delay(200);
+			}
+		}
 	}
 }
+/*
+Producer(Serialize) --> Kafka Broker --> Consumer(Deserialize)
+Kafka broker ise mesajlari once ram'e sonra da diske kaydeder ya da;
+zero copy ile ram olmadan dogrudan diske de kaydedebilir daha performansli ancak ssl olmamasi lazim 
+
+Message 2 parcadan olusur (key, value) key null olursa mesaj rastgele bir partition'a gider 
+key degeri verilirse ilgili hash algoritmasından sonra ilgili partitiona gider ve yine aynı keye sahip bir mesaj gonderilirse o da aynı partitiona gider
+key ve value binary seklinde serialize edilir okunurkende record'da deserilize edilir
+*/
--- Kafka.Producer/Program.cs
+++ Kafka.Producer/Program.cs
@@ -5,11 +5,14 @@ namespace Kafka.Producer
 {
 	internal class Program
 	{
-		string topicName = "topic_one";
 		static async Task Main(string[] args)
 		{
 			var kafkaService = new KafkaService();
-			await kafkaService.CreateTopicAsync();
+			var topicName = "use-case-1-topic";
+			await kafkaService.CreateTopicAsync(topicName);
+			await kafkaService.SendSimpleMessageWithNullKey(topicName);
+
+			Console.WriteLine("mesajlar gonderilmistir");
 		}
 	}
 }
